7eb5138adbf50afe74191073726508bc71e25cbc
PUPIER-VALADE Florentin <gamer.florentin@gmail.com>
2026-01-22 17:25:18 +0100

    Create head.blade.php

---
+++ resources/views/custom/head.blade.php
@@ -0,0 +1,83 @@
+<style>
+    .footer--links-item a i {
+        color: #FFCA55 !important;
+    }
+
+    i.bi.bi-chevron-left.btn.btn-outline-primary,
+    i.bi.bi-chevron-right.btn.btn-outline-primary {
+        border-color: #FFCA55;
+        background-color: #FFCA55;
+        color: #FFFFFF;
+    }
+
+    i.bi.bi-chevron-left.btn,
+    i.bi.bi-chevron-right.btn {
+        padding: .5rem;
+    }
+
+    .glide__bullets.bottom-0 {
+        bottom: -1.5rem !important;
+    }
+
+    .img-hover-zoom.img-hover-zoom--colorize.w-100 {
+        margin-top: 1.5rem;
+    }
+
+    img.shadow {
+        border-radius: 50%;
+    }
+
+    img.img-fluid.w-100.background--height-custom {
+        filter: blur(.5rem) brightness(.5);
+        border-bottom: 4px solid var(--color-primary);
+    }
+
+    #staff .img-hover-zoom--colorize:hover img {
+        transform: none !important;
+    }
+
+    a.btn.btn-primary.btn-block.navbar-brand {
+        box-shadow: none !important;
+    }
+
+    .row.justify-content-center.align-items-center.w-100 {
+        padding: 10%;
+    }
+
+    #staff {
+        padding-top: 4rem;
+    }
+
+    .row.justify-content-center.mt-5 {
+        row-gap: 25px;
+    }
+
+    .payment-method img.img-fluid {
+        margin: auto;
+    }
+
+    #notificationsCounter {
+        margin-left: 2.5px;
+    }
+
+    .me-3 {
+        display: none;
+        margin-right: none !important;
+    }
+
+    .modal-footer .form-check .form-check-label a {
+        text-decoration: underline;
+    }
+
+    .container--rainbow js-particles-trigger position-relative+h1 {
+        padding-top: 165px;
+    }
+</style>
+
+<script defer data-domain="pixelax.fr"
+    src="https://plausible.pixelax.fr/js/script.file-downloads.hash.outbound-links.js"></script>
+<script>
+    window.plausible = window.plausible || function() {
+        (window.plausible.q = window.plausible.q || []).push(arguments)
+    }
+</script>
